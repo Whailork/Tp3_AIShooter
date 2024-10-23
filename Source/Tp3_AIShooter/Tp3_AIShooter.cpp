d0e569abc71ad68f4c899071097d65371bc6063a
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-10-23 12:48:05 -0400

    push avec le contenu demo du prof

--- Source/Tp3_AIShooter/Tp3_AIShooter.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Tp3_AIShooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tp3_AIShooter, "Tp3_AIShooter" );
- 
