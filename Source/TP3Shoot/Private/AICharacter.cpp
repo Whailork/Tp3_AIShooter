03fd157a978479364d3b8929b36b51f02041ef14
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-10-26 14:53:45 -0400

    la health bar est linké à la variable de l'ai

--- Source/TP3Shoot/Private/AICharacter.cpp
+++ Source/TP3Shoot/Private/AICharacter.cpp
@@ -49,12 +49,15 @@ AAICharacter::AAICharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+	
 }
 
 // Called when the game starts or when spawned
 void AAICharacter::BeginPlay()
 {
 	Super::BeginPlay();
+	Health = StartingHealth;
 	
 }
 
